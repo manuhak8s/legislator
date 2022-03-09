7570ab02dd06da120ac6e36926235c529df198af
manuhak8s <manuel.haugg.business@gmail.com>
2022-03-09 10:28:20 +0100

    created apply and destroy command

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -1,1 +1,14 @@
 package cmd
+
+import (
+	_"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var destroy = &cobra.Command{
+	Use: "destroy",
+	Short: "Removes the constitution config from the kubernetes cluster.",
+	Long: "All network policies become removed from the kubernetes cluster based on their configuration.",
+	Run: nil,
+}
