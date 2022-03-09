7e2c9443f8dbc7f751c54ef3f3d7b7c4b1579cdd
manuhak8s <manuel.haugg.business@gmail.com>
2022-03-09 17:09:31 +0100

    demo command with kubernetes connection

---
+++ cmd/namespaces.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"context"
+	"github.com/spf13/cobra"
+	"github.com/manuhak8s/legislator/pkg/k8s"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var namespaceCmd = &cobra.Command{
+	Use: "namespaces",
+	Short: "Get namespaces from kubernetes cluster",
+	Long: "Get namespaces from kubernetes cluster",
+	Run: getNamespaceData,
+}
+
+func getNamespaceData(cmd *cobra.Command, args []string) {
+	//clientset, err := k8s.GetK8sClient()
+	clientset, err := k8s.GetK8sDefaultClient()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	namespaces, err := clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, ns := range namespaces.Items {
+		fmt.Println(ns.Name)
+	}
+}
