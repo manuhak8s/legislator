af8923cb083fbb34780d1feb63555f7e48ea3813
manuhak8s <manuel.haugg.business@gmail.com>
2022-04-01 16:43:23 +0200

    modified data models

--- pkg/config/selector.go
+++ pkg/config/selector.go
@@ -1,7 +1,10 @@
 package config
 
-type PodSelector []struct {
-	// list of elements that should be selected by the selector
-	PodName string `yaml:"podName"`
-	PodExpression string `yaml:"podExpression"`
+// PodSelector represents the required fields for selecting pods
+type PodSelector struct {
+    // Map of key-value-pairs that shall translated to a network policy
+    MatchLabels map[string]string `yaml:"matchLabels"`
+
+    // String slice that should allow to select multiple pods by a defined naming convention
+    MatchExpressions []string `yaml:"matchExpressions"`
 }
