dcb54a7bba14671283efc6501c2c33a433a75e7a
manuhak8s <manuel.haugg.business@gmail.com>
2022-05-05 13:37:07 +0200

    main

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"github.com/manuhak8s/legislator/cmd"
+	_"fmt"
+
+	_ "github.com/k0kubun/pp"
+
+	_ "github.com/manuhak8s/legislator/cmd"
+	_ "github.com/manuhak8s/legislator/pkg/config"
+	_ "github.com/manuhak8s/legislator/pkg/k8s"
+	"github.com/manuhak8s/legislator/pkg/luther"
+	//v1 "k8s.io/api/networking/v1"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
-	cmd.Execute()
+	
+	//luther.DestroyAllNetworkPolicies()
+	
+	//luther.ExecuteLegislation("test_data/configs/v3_data.yaml")
+	//luther.ExecuteLegislation("2_test.constitution.yaml")
+
+	//luther.ExecuteDestruction("test_data/configs/v3_data.yaml")
+	luther.ExecuteDestruction("2_test.constitution.yaml")
+	
+	//cmd.Execute()
  }
