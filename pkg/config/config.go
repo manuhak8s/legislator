ae20806901350a410292fa1366f438d8205766ef
manuhak8s <manuel.haugg.business@gmail.com>
2022-06-07 21:44:06 +0200

    .

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,9 +1,10 @@
 package config
 
 import (
-	_"fmt"
+	"fmt"
 	"io/ioutil"
 
+    "github.com/manuhak8s/legislator/pkg/logger"
 	"gopkg.in/yaml.v2"
 )
 
@@ -30,5 +31,64 @@ func (config *Config) ReadConfig(configPath string) (*Config, error) {
         return nil, err
     }
 
+    logger.TriggerOutput("loading", "... validating config file ... ")
+    err = ValidateLegislatorConfig(config, configPath)
+    if err != nil {
+        return nil, err
+    }
+    logger.TriggerOutput("success", "config validation successfully finished :)")
     return config, nil
+}
+
+func ValidateLegislatorConfig(config *Config, configPath string) error {
+    if config.ConnectedSets == nil {
+        return fmt.Errorf("error: no connectedSets field defined at config: %s - please read the legislator instructions for further information", configPath)
+    }
+    
+    var setNames []string
+    for _, set := range config.ConnectedSets {
+        setNames = append(setNames, set.Name)
+    }
+
+    err := checkForDuplicates(setNames)
+    if err != nil {
+        return err
+    }
+
+    for _, set := range config.ConnectedSets {
+        err = checkForConnectedSetObligationFields(set, configPath)
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
+
+func checkForDuplicates(stringSlice []string) error {
+    allKeys := make(map[string]bool)
+    for _, item := range stringSlice {
+        _, exist := allKeys[item]
+        if exist {
+            return fmt.Errorf("duplicate connected set name found: %s - duplicates are not allowed as set names, for further information read the legislator instructions", item)
+        }
+    }
+
+    return nil
+}
+
+func checkForConnectedSetObligationFields(connectedSet ConnectedSet, configPath string) error {
+    if connectedSet.Name == "" {
+        return fmt.Errorf("obligation field error: no name defined for connected set at config: %s - a name has to be set for a connected set, for further information read the legislator instructions", configPath)
+    }
+
+    if connectedSet.TargetNamespaces.MatchLabels == nil && connectedSet.TargetNamespaces.MatchExpressions == nil {
+        return fmt.Errorf("obligation field error: no selector defined for target namespaces at config: %s - a selector has to be defined for a connected set, for further information read the legislator instructions", configPath)
+    }
+
+    /*if connectedSet.PodSelector.MatchLabels == nil && connectedSet.PodSelector.MatchExpressions == nil {
+        return fmt.Errorf("obligation field error: no selector defined target pods at config: %s - a selector has to be defined for a connected set, for further information read the legislator instructions", configPath)
+    }*/
+
+    return nil
 }
